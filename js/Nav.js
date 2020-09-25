6414d3fc606627d2158c2362501a5b408f97758c
Kynan Stewart Hughes <mail@kynan.net.au>
2020-09-25 14:44:20 +1000

    v0.17 - Background image on the Start view

--- js/Nav.js
+++ js/Nav.js
@@ -29,7 +29,7 @@ const Nav = (props /*: Props */) => {
 					<${Link}
 						activeClassName="${styles.activeNavItem}"
 						href="/">
-							v0.16
+							v0.17
 					</${Link}>
 				</li>
 				<li>
--- js/Start.js
+++ js/Start.js
@@ -1,23 +1,18 @@
 // @flow
 import { h, render } from "../web_modules/preact.js";
 import { Router, Link } from "../web_modules/preact-router.js";
-import { useState, useEffect } from "../web_modules/preact/hooks.js";
 import { createStyles, rawStyles } from "../web_modules/simplestyle-js.js";
 import screenfull from "../web_modules/screenfull.js";
 import htm from "../web_modules/htm.js";
-import Nav from "./Nav.js";
-import Form from "./Form.js";
-import FormInputLat from "./FormInputLat.js";
-import FormInputLng from "./FormInputLng.js";
-import FormInputElevation from "./FormInputElevation.js";
-import FormInputScale from "./FormInputScale.js";
-import { setAutoFreeze } from "../web_modules/immer.js";
 
 const html = htm.bind(h);
 rawStyles({});
 const [styles] = createStyles({
     startContainer: {
         height: "100%",
+        backgroundImage: "url(/img/dbs_background.2.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
     },
     contentContainer: {
         height: "100%",
@@ -29,6 +24,8 @@ const [styles] = createStyles({
         margin: "3rem",
     },
     startChild: {
+        color: "white",
+        fontSize: "1.2rem",
         margin: "0 auto",
     },
 });
@@ -39,16 +36,16 @@ type Props = {
 */
 const Start = (props /*: Props */) => {
     //
-    useEffect(() => {});
-
     return html`
         <div class="${styles.startContainer}">
             <div class="${styles.contentContainer}">
                 <div class="${styles.startChild}">
                     <p>
-                        Welcome! Please point your phone at Parliament House and
-                        press "Start" below to see the augmented-reality
-                        artwork.
+                        Don't be scared!
+                    </p>
+                    <p>
+                        Please point your phone at Parliament House and press
+                        "Start" below to see the augmented-reality artwork.
                     </p>
                 </div>
                 <div class="${styles.startChild}">
